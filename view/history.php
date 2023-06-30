6131c28390dc2127d888098b872ddf3511882216
goddod <128914033+goddod@users.noreply.github.com>
2023-06-29 23:10:43 -0700

    init code
    add

--- view/history.php
+++ view/history.php
@@ -1,21 +1,33 @@
-<div>
-<div class="scrollmenu" style="display: flex; font-size:12px;">
-<div style="margin-left:1%; display: flex; margin-bottom: 5%;"><a style=" font-size: 17px; background-color: rgba(0, 66, 0, 0.766);"  href="#" onclick="openPopup()"><img src="calendar-plus-svgrepo-com.png" width="25" height="25"></a><a href="#" onclick="openPopup()" style="font-size: 17px; color: white;">BORROW</a></div>
-<div style="margin-left:1%; display: flex; margin-bottom: 5%;"><a style=" font-size: 17px; background-color: rgba(0, 66, 0, 0.766);" href="?option=book"><img src="table-add-svgrepo-com.png" width="25" height="25"></a><a style= "font-size: 17px; color: white;" href="?option=book">MANAGE</a></div>
+<div class="background--button">
+    <div class="scrollmenu">
+
+        <button class="btn-person">
+            <a class="btn-add" href="#" onclick="openPopup()"><img class="icon--img" src="calendar-plus-svgrepo-com.png"
+                    width="25" height="25"></a>
+            <a class="title--btn" href="#" onclick="openPopup()">BORROW</a>
+        </button>
+        <button class="btn-person">
+            <a class="btn-add" href="?option=book"><img class="icon--img" src="table-add-svgrepo-com.png" width="25"
+                    height="25"></a>
+            <a class="title--btn" href="?option=book">MANAGE</a>
+        </button>
+    </div>
+    <div class="history">
+        <form method="post" class="form1">
+            <input class="inputsearch1" type="search" name="username"><button class="button"><img
+                    src="search-magnifying-glass-svgrepo-com (1).png" width="25" height="25"></button>
+            <!-- <input style="font-size: 27px;" type="image" src="search-square-svgrepo-com.png" alt="Submit" width="45" height="45" name="search"> -->
+
+            <input class="inputsubmit1" type="submit" name="delete" value="DELETE">
+        </form>
+
+    </div>
+    <div class="image1">
+        <img src="Screenshot 2023-06-23 075530.png" width="1750" height="750">
+    </div>
 </div>
-<div class="history" style="margin-top:-5%;">
-            <form method="post" class="form1">
-                <input class="inputsearch1" type="search"
-                    name="username"><button class="button"><img src="search-magnifying-glass-svgrepo-com (1).png" width="25" height="25"></button>
-                    <!-- <input style="font-size: 27px;" type="image" src="search-square-svgrepo-com.png" alt="Submit" width="45" height="45" name="search"> -->
-                    
-                    <input class="inputsubmit1" type="submit" name="delete" value="DELETE">
-            </form>
-                
-</div>
-<div class="image1">
-    <img src="Screenshot 2023-06-23 075530.png" width="1750" height="750">
-</div>
-</div>
-<div style="margin-left:1%; display: flex; margin-bottom: 2%;"><a style=" font-size: 17px; background-color: rgba(0, 66, 0, 0.766);" href="?option=person"><img src="table-svgrepo-com.png" width="25" height="25"></a><a style= "font-size: 17px; color: white;" href="?option=person">MANAGE</a></div>
-  <!-- <a href="?option=book"><img src="book-svgrepo-com.png" wwidth="25" height="25"></a> -->
+<div style="margin-left:1%; display: flex; margin-bottom: 2%;"><a
+        style=" font-size: 17px; background-color: rgba(0, 66, 0, 0.766);" href="?option=person"><img
+            src="table-svgrepo-com.png" width="25" height="25"></a><a style="font-size: 17px; color: white;"
+        href="?option=person">MANAGE</a></div>
+<!-- <a href="?option=book"><img src="book-svgrepo-com.png" wwidth="25" height="25"></a> -->
--- view/person.php
+++ view/person.php
@@ -1,31 +1,32 @@
-<div style="background-color: rgb(187, 194, 200);">
-<div class="scrollmenu" style="display: flex;">
-  <div style="display: flex;
-  align-items: center; background-color: green  ;"><a style=" font-size: 17px; background-color: rgba(0, 66, 0, 0.766);" href="#" onclick="openPopup()"><img src="user-add-svgrepo-com.png" width="25" height="25"></a><a style=" font-size: 17px; color: white;" href="#" onclick="openPopup()">ADD</a></div>
-  <div style="margin-left:1%; display: flex; margin-bottom: 2%; margin-top:1%;"><a style=" font-size: 17px; background-color: rgba(0, 66, 0, 0.766);" href="?option=person"><img src="table-svgrepo-com (1).png" width="25" height="25"></a><a style="font-size: 17px; color: white;" href="?option=person">MANAGE</a></div>
-  <!-- <a href="?option=book"><img src="book-svg
-  repo-com.png" wwidth="25" height="25"></a> -->
-    <button class="btn-person">
-        <a class="btn-add" href="#" onclick="openPopup()"><img class="icon--img" src="user-add-svgrepo-com.png" width="25" height="25"></a>
-        
-        <a class="title--btn" href="#" onclick="openPopup()">ADD</a>
-    </button>
-</div>
+<div class="background--button">
+    <div class="scrollmenu">
 
-<div style="margin-top: -2.7%;">
-            <form method="post" class="form">
-                <input class="inputsearch" type="search"
-                    name="username"><button class="button"><img src="search-magnifying-glass-svgrepo-com (1).png" width="25" height="25"></button>
-                    <!-- <input style="font-size: 27px;" type="image" src="search-square-svgrepo-com.png" alt="Submit" width="45" height="45" name="search"> -->
-                    
-                    <input class="inputsubmit" type="submit" name="delete" value="DELETE">
-            </form>
-                
-</div>
-<p></p>
-<div class="image">
-    <img src="Screenshot 2023-06-22 224226.png" width="1365" height="650">
-</div>
+        <button class="btn-person">
+            <a class="btn-add" href="#" onclick="openPopup()"><img class="icon--img" src="user-add-svgrepo-com.png"
+                    width="25" height="25"></a>
+            <a class="title--btn" href="#" onclick="openPopup()">ADD</a>
+        </button>
+        <button class="btn-person">
+            <a class="btn-add" href="?option=person"><img class="icon--img" src="table-svgrepo-com (1).png" width="25"
+                    height="25"></a>
+            <a class="title--btn" href="?option=person">MANAGE</a>
+        </button>
+    </div>
+
+    <div class="history">
+        <form method="post" class="form1">
+            <input class="inputsearch1" type="search" name="username"><button class="button"><img
+                    src="search-magnifying-glass-svgrepo-com (1).png" width="25" height="25"></button>
+            <!-- <input style="font-size: 27px;" type="image" src="search-square-svgrepo-com.png" alt="Submit" width="45" height="45" name="search"> -->
+
+            <input class="inputsubmit1" type="submit" name="delete" value="DELETE">
+        </form>
+
+    </div>
+    <p></p>
+    <div class="image">
+        <img src="Screenshot 2023-06-22 224226.png" width="1365" height="650">
+    </div>
 </div>
 
 <div class="popup-wrapper" id="popup-wrapper" style="text-align: center;">
@@ -52,4 +53,3 @@
         </form>
     </div>
 </div>
-
